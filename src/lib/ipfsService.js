b2f029f3f0ab659a57871dc7d4b4129a104a6bfa
Danielius Dvareckas <danielius@hotmail.com>
2018-11-29 02:20:39 +0200

    FIX: Ipfs call.

--- src/lib/ipfsService.js
+++ src/lib/ipfsService.js
@@ -4,7 +4,7 @@ const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https
 
 export const add = async (files) => {
     try {
-        const hash = await ipfs.files.add(files, { recursive: false });
+        const hash = await ipfs.add(files, { recursive: false });
         return hash;
     } catch (err) {
         console.log('add: ', files, err);
